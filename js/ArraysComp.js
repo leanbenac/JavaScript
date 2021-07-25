291e2094ebe8b19d6388f5a77622ab50af52d721
leanbenac <benacleandro@gmail.com>
2021-07-25 11:17:03 -0300

    arraycomp

--- js/ArraysComp.js
+++ js/ArraysComp.js
@@ -1,19 +1,19 @@
 /* desafio */
 
 const arrayMotos =[
-    { moto: "honda", cc: "1000", vel: "300km/h"},
-    { moto: "yamaha", cc: "1200", vel: "400km/h"},
-    { moto: "ktm", cc: "1100", vel: "350km/h"},
-    { moto: "benelli", cc: "700", vel: "280km/h"},
-    { moto: "zanella", cc: "800", vel: "290km/h"},
+    { moto: "honda", cc: 1000, vel: "300km/h", precio: 200 },
+    { moto: "yamaha", cc: 1200, vel: "400km/h", precio: 250},
+    { moto: "ktm", cc: 1100, vel: "350km/h", precio: 210},
+    { moto: "benelli", cc: 700, vel: "280km/h", precio: 170},
+    { moto: "zanella", cc: 800, vel: "290km/h", precio: 190},
 ]
 
 arrayMotos.sort((a,b) => {
-    if (a.vel < b.vel){
+    if (a.precio < b.precio){
     return -1;
     }
 
-    if (a.vel > b.vel){
+    if (a.precio > b.precio){
         return 1;
     }
 
